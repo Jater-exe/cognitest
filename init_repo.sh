3d361887fba3feb370bbbb3db45b4f06767b65fd
Jater-exe <jter6868@gmail.com>
2025-12-12 21:23:53 +0100

    initialize

---
+++ init_repo.sh
@@ -0,0 +1,7 @@
+echo "# cognitest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Jater-exe/cognitest.git
+git push -u origin main
