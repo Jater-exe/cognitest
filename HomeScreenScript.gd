f4ae5ba62463798e4464be79ea647afa93e08a8e
MarcelRochera00 <marcelrochera9@gmail.com>
2025-12-12 22:53:22 +0100

    Folders

--- HomeScreenScript.gd
+++ HomeScreenScript.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_objective__button_pressed() -> void:
-	get_tree().change_scene_to_file("res://objective_control.tscn")
+	get_tree().change_scene_to_file("res://Scenes/objective_control.tscn")
