a29e84178a17814cfc03ad8eee1a3899054b802d
MarcelRochera00 <marcelrochera9@gmail.com>
2025-12-13 11:46:13 +0100

    Main

--- Scenes/objective___button.gd
+++ Scenes/objective___button.gd
@@ -1,21 +1,7 @@
 extends TextureButton
 
 func _ready():
-	pivot_offset = size / 2
-
-func _on_button_down():
-	var tween = create_tween()
-	tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.08)\
-		.set_trans(Tween.TRANS_QUAD)\
-		.set_ease(Tween.EASE_OUT)
-
-func _on_button_up():
-	var tween = create_tween()
-	tween.tween_property(self, "scale", Vector2(1.05, 1.05), 0.08)\
-		.set_trans(Tween.TRANS_QUAD)\
-		.set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "scale", Vector2(1, 1), 0.08)
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
