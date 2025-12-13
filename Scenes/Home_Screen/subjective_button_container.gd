1d4473f9d6652ad2f304285c311ac543cc411b1d
MarcelRochera00 <marcelrochera9@gmail.com>
2025-12-13 12:08:09 +0100

    Main push

--- Scenes/Home_Screen/subjective_button_container.gd
+++ Scenes/Home_Screen/subjective_button_container.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	center_pivot = size / 2.0
 	pivot_offset = center_pivot
 	scale = Vector2.ONE	
-	var texture_button = get_node_or_null("Objective - Button")
+	var texture_button = get_node_or_null("Subjective - Button")
 	if texture_button:
 		texture_button.button_down.connect(_on_texture_button_down)
 		texture_button.button_up.connect(_on_texture_button_up)
@@ -31,5 +31,5 @@ func _on_texture_button_up() -> void:
 	tween = create_tween()	
 	tween.tween_property(self, "scale", Vector2.ONE, anim_duration).set_ease(Tween.EASE_OUT)
 
-func _on_objective__button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/objective_control.tscn")
+func _on_subjective__button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Subjective_Control/subjective_control.tscn")
--- Scenes/objective___button.gd
+++
@@ -1,8 +0,0 @@
-extends TextureButton
-
-func _ready():
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
