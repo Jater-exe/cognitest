1c7871bc8475c92d0fbe14abe7e192a0b55b06a0
MarcelRochera00 <marcelrochera9@gmail.com>
2025-12-13 20:07:04 +0100

    Ns ja

--- Scenes/Home_Screen/animation_title.gd
+++ Scenes/Home_Screen/animation_title.gd
@@ -1,9 +1,6 @@
 extends AnimationPlayer
 
-@onready var animation_player: AnimationPlayer = $AnimationTitle
-
 func _ready() -> void:
-	if is_instance_valid(animation_player):
-		animation_player.call_deferred("play", "Idle_Title_Animation") 
-	else:
-		push_error("AnimationPlayer reference is invalid!")
+	# Since the script is attached to the AnimationPlayer node,
+	# we can call methods on 'self' (the node itself).
+	play("Idle_Title_Animation")
